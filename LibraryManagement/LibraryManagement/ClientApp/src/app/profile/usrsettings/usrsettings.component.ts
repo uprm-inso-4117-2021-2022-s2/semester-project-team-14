385fadc0fbac71f62b4540537cc4ae9a113859e3
Raynell <raynell.gonzalez@upr.edu>
2022-04-28 00:03:17 -0400

    Added usersettings page and button

---
+++ LibraryManagement/LibraryManagement/ClientApp/src/app/profile/usrsettings/usrsettings.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-usrsettings',
+  templateUrl: './usrsettings.component.html',
+  styleUrls: ['./usrsettings.component.scss']
+})
+export class UsrsettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
